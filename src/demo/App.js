0d9b42a7c84fe70a7a2efe5adc0373809bb79bc6
gurpreetbirdi <birdi.gpsingh@gmail.com>
2018-03-23 17:09:16 +0530

    global-store

---
+++ src/demo/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    Global-Store
+  </div>
+);
+
+export default App;
---
+++ src/lib/index.js
@@ -0,0 +1,11 @@
+// import Example from './components/Example';
+// import SecondExample from './components/SecondExample';
+import { update } from './actions'
+import { updateText } from './reducers'
+import { globalStore } from './store'
+
+// export default Example;
+
+export {
+  update, updateText, globalStore,
+};
